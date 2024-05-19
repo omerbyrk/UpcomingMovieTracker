60299cab5a79e1f5d4fc00e39785079c5f96bf0e
Ömer Bayrak <omersocial8@gmail.com>
2024-05-19 13:18:37 +0300

    added search field code

--- UpcomingMovieTracker/Modules/Home/HomeViewController.swift
+++ UpcomingMovieTracker/Modules/Home/HomeViewController.swift
@@ -13,6 +13,7 @@ class HomeViewController: UIViewController {
 
     @IBOutlet weak var bannerCollection: HomeBannerCollection!
 
+    @IBOutlet weak var searchField: UITextField!
     @IBOutlet weak var movieSectionTable: MovieSectionTable!
     @IBOutlet weak var bannerDotCollection: HomeBannerDotCollection!
     @IBOutlet weak var userAvatar: UIImageView!
@@ -20,7 +21,7 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         userAvatar.layer.cornerRadius = userAvatar.frame.height / 2
-        
+        designSearchField()
         Task {
             await bannerCollection.configure()
             bannerDotCollection.configure(homeBannerCollection:  bannerCollection)
@@ -29,6 +30,27 @@ class HomeViewController: UIViewController {
             
 
         }
+    }
+    
+    func designSearchField() {
+        let search = UIButton(type: .custom)
+        search.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        search.tintColor = .darkGray
+        search.configuration = UIButton.Configuration.plain()
+        search.configuration?.imagePadding = 2.5
+        
+        searchField.leftView = search
+        searchField.leftViewMode = .always
+        
+        
+        let filter = UIButton(type: .custom)
+        filter.setImage(UIImage(systemName: "line.3.horizontal.decrease.circle"), for: .normal)
+        filter.tintColor = .darkGray
+        filter.configuration = UIButton.Configuration.plain()
+        filter.configuration?.imagePadding = 2.5
+        
+        searchField.rightView = filter
+        searchField.rightViewMode = .always
     }
 }
 
